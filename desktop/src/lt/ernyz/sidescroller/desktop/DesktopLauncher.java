efbcd947cb9e23d1615105bdf0bd7f69741e67d8
Ernyz <ernestas.zaloga@gmail.com>
2016-08-30 15:44:12 +0300

    Initial commit

---
+++ desktop/src/lt/ernyz/sidescroller/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lt.ernyz.sidescroller.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lt.ernyz.sidescroller.SidescrollerTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SidescrollerTest(), config);
+	}
+}
