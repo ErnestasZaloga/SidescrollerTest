8d705ed06e388b8c3e6a164481425eaa12b4d2fe
Ernyz <ernestas.zaloga@gmail.com>
2016-08-30 21:15:37 +0300

    Started working on gravity and collisions

--- core/src/lt/ernyz/sidescroller/Player.java
+++ core/src/lt/ernyz/sidescroller/Player.java
@@ -1,11 +1,27 @@
 package lt.ernyz.sidescroller;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 
 public class Player extends Entity {
+	
+	private Vector2 velocity = new Vector2(0f, 0f);
 
 	public Player(Texture texture, float x, float y) {
 		super(texture, x, y);
+	}
+
+	public void update(float delta) {
+		this.x += velocity.x * delta;
+		this.y += velocity.y * delta;
+	}
+	
+	public Vector2 getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(Vector2 velocity) {
+		this.velocity = velocity;
 	}
 
 }
--- core/src/lt/ernyz/sidescroller/SidescrollerTest.java
+++ core/src/lt/ernyz/sidescroller/SidescrollerTest.java
@@ -2,12 +2,15 @@ package lt.ernyz.sidescroller;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 
 public class SidescrollerTest extends ApplicationAdapter {
+	private final float GRAVITY = -9.8f; 
+	
 	private Player player;
 	private Array<Entity> tiles = new Array<Entity>();
 	private SpriteBatch batch;
@@ -19,9 +22,21 @@ public class SidescrollerTest extends ApplicationAdapter {
 	}
 
 	@Override
-	public void render () {
-		//Update logic
-		//...
+	public void render() {
+		if(Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
+			Gdx.app.exit();
+		}
+		
+		float delta = Gdx.graphics.getDeltaTime();
+		/*Update world*/
+		//Apply gravity
+		player.getVelocity().y += GRAVITY;
+		
+		//Update entities
+		player.update(delta);
+		
+		//Check collisions
+		checkCollisions();
 		
 		//Render
 		Gdx.gl.glClearColor(0, 0, 1, 1);
@@ -35,9 +50,47 @@ public class SidescrollerTest extends ApplicationAdapter {
 	}
 	
 	private void setupLevel() {
-		player = new Player(new Texture("player.png"), 100, 100);
+		player = new Player(new Texture("player.png"), 100, 500);
 		for(int i = 0; i < 15; i++) {
 			tiles.add(new Entity(new Texture("grass.png"), i*32, 0));
+		}
+	}
+	
+	private void checkCollisions() {
+		for(Entity t : tiles) {
+			float intersectionW = 0f;
+			float intersectionH = 0f;
+			
+			if(((t.getX() < player.getX() && player.getX() < t.getX() + t.getWidth()) || (t.getX() < player.getX() + player.getWidth() && player.getX() + player.getWidth() < t.getX() + t. getWidth()))
+					&& ((t.getY() + t.getHeight() < player.getY() && player.getY() + player.getHeight() < t.getY()) || (t.getY() + t.getHeight() < player.getY() && player.getY() < t.getY() + t.getHeight()))) {
+				System.out.println("intersection!");
+				if(player.getX() < t.getX() + t.getWidth()) {
+					intersectionW = t.getX() + t.getWidth() - player.getX();
+				} else if(player.getX() + player.getWidth() > t.getX()) {
+					intersectionW = player.getX() + player.getWidth() - t.getX();
+				}
+				
+				if(player.getY() + player.getHeight() > t.getY()) {
+					intersectionH = player.getY() + player.getHeight() - t.getY();
+				} else if(player.getY() < t.getY() + t.getHeight()) {
+					intersectionH = t.getY() + t.getHeight() - player.getY();
+				}
+				
+				if(intersectionW >= intersectionH) {
+					if(player.getX() < t.getX() + t.getWidth()) {
+						player.setX(player.getX() + intersectionW);
+					} else if(player.getX() + player.getWidth() > t.getX()) {
+						player.setX(player.getX() - intersectionW);
+					}
+				} else {
+					if(player.getY() + player.getHeight() > t.getY()) {
+						player.setY(player.getY() - intersectionH);
+					} else if(player.getY() < t.getY() + t.getHeight()) {
+						player.setY(player.getY() + intersectionH);
+					}
+					player.getVelocity().y = 0f;
+				}
+			}
 		}
 	}
 	
